7cb2fda6a430b732a37cfc61a25846b94b84af05
Kamil <kamiljanprzybyl@gmail.com>
2020-06-09 12:33:06 +0200

    header update

---
+++ includes/push_swap.h
@@ -0,0 +1,9 @@
+#include "../libft/includes/libft.h"
+#include <stdio.h>
+
+
+
+int push_swap(int ac, char **av);
+void swap(t_list **lst);
+void rotate(t_list **lst);
+void reverse_rotate(t_list **lst);
