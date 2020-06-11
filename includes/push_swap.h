ab80d662802032a9860fe55648e66d971205a5cb
Kamil <kamiljanprzybyl@gmail.com>
2020-06-11 09:22:49 +0200

    nothing changed

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,9 +1,11 @@
 #include "../libft/includes/libft.h"
+// #include "../ft_printf/includes/ft_printf.h"
 #include <stdio.h>
 
 
-
 int push_swap(int ac, char **av);
+void ft_lstprint_two(t_list *a, t_list *b);
 void swap(t_list **lst);
 void rotate(t_list **lst);
 void reverse_rotate(t_list **lst);
+void push(t_list **from, t_list **to);
